baacea3b8daf66819b986abac5b7c0924136e380
Uleev <uleev777@yandex.ru>
2022-09-16 09:45:36 +0400

    В работе

--- src/main/java/example/Task5.java
+++ src/main/java/example/Task5.java
@@ -1,20 +1,25 @@
 package example;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 //Реализовать функцию, которая для данного массива целых чисел(Integer) возвращает число наиболее близкое к 10,
 //если таких чисел два(н-р 8,12) то возвращать больше из них
 public class Task5 {
     public static int returnNearestNumber(int[] numbers) {
         int min = Integer.MAX_VALUE;
         int desiredNumber = 10;
-        int nearestNumber = 0;
+        List<Integer> nearestNumber = null;
 
-        for (Integer number : numbers) {
+        for (int number : numbers) {
             int diff = Math.abs(desiredNumber - number);
             if (diff <= min) {
                 min = diff;
-                nearestNumber = number;
             }
+            nearestNumber = new ArrayList<>();
+            nearestNumber.add(number);
         }
-        return nearestNumber;
+        return Collections.max(nearestNumber);
     }
 }
