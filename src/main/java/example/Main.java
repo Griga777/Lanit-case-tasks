79e349f8a88ec921f10abe5917e443797ff4a061
Uleev <uleev777@yandex.ru>
2022-09-21 00:08:13 +0400

    Готова 1 часть задания

--- src/main/java/example/Main.java
+++ src/main/java/example/Main.java
@@ -8,7 +8,7 @@ public class Main {
         System.out.println("======================");
         System.out.println(Task3.permutation("я люблю пиво!", "пиво люблю я!"));
         System.out.println("======================");
-        Task4.outputWordOrNumber(91);
+        Task4.outputWordOrNumber(10);
         System.out.println("======================");
         int[] numbers = {8, 22, 12};
         System.out.println(Task5.returnNearestNumber(numbers));
--- src/main/java/example/Task4.java
+++ src/main/java/example/Task4.java
@@ -4,12 +4,12 @@ package example;
 //печатает «bar», если это число делится на 5 без остатка, печатает «foobar», если это число делится на 15 без остатка, печатает само число в остальных случаях.
 public class Task4 {
     public static void outputWordOrNumber(int number) {
-        if (number % 3 == 0) {
+        if (number % 15 == 0) {
+            System.out.println("foobar");
+        } else if (number % 3 == 0) {
             System.out.println("foo");
         } else if (number % 5 == 0) {
             System.out.println("bar");
-        } else if (number % 15 == 0) {
-            System.out.println("foobar");
         } else {
             System.out.println(number);
         }
