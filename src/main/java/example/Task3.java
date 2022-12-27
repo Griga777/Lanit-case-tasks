7766955024075fef8bf313a7e013b1839704beb2
Uleev <uleev777@yandex.ru>
2022-12-27 23:40:07 +0400

    В работе

--- src/main/java/example/Task3.java
+++ src/main/java/example/Task3.java
@@ -1,11 +1,13 @@
 package example;
 
+import java.util.Arrays;
+
 //Реализуйте метод, определяющий, является ли одна строка перестановкой другой. Под перестановкой понимаем любое изменение порядка символов.
 //Регистр учитывается, пробелы являются существенными.
 public class Task3 {
     public static String sort(String s) {
         char[] content = s.toCharArray();
-        java.util.Arrays.sort(content);
+        Arrays.sort(content);
         return new String(content);
     }
 
