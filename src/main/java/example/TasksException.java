ea2ff937bc54faa367f3e42a6b10ce09249f300d
Uleev <uleev777@yandex.ru>
2023-05-11 09:33:14 +0400

    Удалил лишнее

--- src/main/java/example/TasksException.java
+++ src/main/java/example/TasksException.java
@@ -3,16 +3,8 @@ package example;
 public class TasksException extends RuntimeException {
     private static final String MESSAGE = "Ошибка!";
 
-    public TasksException() {
-        super(MESSAGE);
-    }
-
     public TasksException(String str) {
         super(MESSAGE + " " + str);
-    }
-
-    public TasksException(Throwable e) {
-        super(MESSAGE, e);
     }
 
     public TasksException(String str, Throwable e) {
